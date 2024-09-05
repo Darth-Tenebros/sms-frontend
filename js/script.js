68a36b89dd39f00133ea2809bbbcb27eacac9bd9
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-05 15:03:16 +0200

    fetch test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-const links = document.querySelectorAll('.sidebar a');
-const sections = document.querySelectorAll('.main-content div');
+// const links = document.querySelectorAll('.sidebar a');
+// const sections = document.querySelectorAll('.main-content div');
 
-links.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        sections.forEach(section => section.classList.remove('active'));
-        document.querySelector(`.${link.id.split('-')[0]}`).classList.add('active');
+// links.forEach(link => {
+//     link.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         sections.forEach(section => section.classList.remove('active'));
+//         document.querySelector(`.${link.id.split('-')[0]}`).classList.add('active');
+//     });
+// });
+
+const getToken = async function(){
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    const token = await fetch('http://localhost:3000/login/', {
+        method: "POST",
+        body: JSON.stringify({
+            "email": "johnson@example.com",
+            "password": "password1"
+        }),
+        headers: headers
+
     });
-});
+
+    console.log(await token.json());
+}
+
+getToken();
